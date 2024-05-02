0a2c2e125431170d407e3a3e1b7186cc6d43ed4b
fnaeijii <fatemehnaeiji2002@gmail.com>
2024-05-02 21:41:21 +0330

    upload kilid_site project

---
+++ Kilidsite/Controllers/HomeController.cs
@@ -0,0 +1,254 @@
+﻿using Kilidsite.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using System.Xml.Linq;
+using Kilidsite.Models;
+using System.Diagnostics;
+using System.Drawing;
+
+namespace Kilidsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        kilidEntities context = new kilidEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            int homes = context.Homes.Count();
+
+            int agency = context.Agancies.Count();
+
+            int users = context.Users.Count();
+
+            ViewBag.Homes = homes;
+            ViewBag.Agency = agency;
+            ViewBag.Users = users;
+
+
+            return View();
+        }
+
+
+
+        public ActionResult login()
+        {
+
+            return View();
+        }
+
+        
+
+
+        public ActionResult edit_acc()
+        {
+
+            return View();
+        }
+
+        public ActionResult Advertise()
+        {
+
+            return View();
+        }
+
+        public ActionResult AgencyforAdvertise()
+        {
+
+            return View();
+        }
+
+
+
+        public ActionResult agencies()
+        {
+
+
+            return View();
+        }
+
+
+
+        public ActionResult regAgancy()
+        {
+
+            return View();
+         }
+
+        public ActionResult checkagaPhone(string agencyPhone)
+        {
+        
+            var statee = 1;
+            var ag = context.Agancies.Where(x => x.AcancyPhone== agencyPhone).SingleOrDefault();
+            
+            if (ag == null)
+            {
+                statee = 0;
+                
+            }
+            else
+            {
+                Advertise newadvertise = new Advertise();
+                newadvertise.agID = ag.ID;
+                
+                context.Advertises.Add(newadvertise);
+                context.SaveChanges();
+            }
+
+            return Json( statee , JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult filterBuy(int AdType,string place,int minsize, int maxsize, int minage, int maxage, int minprice, int maxprice, int roomnum, int parking, int balcony, int elevator, int stor)
+        {
+            var homes = context.Homes.Where(x => (x.City == place || x.Neighborhood == place)&& parking == x.Parkinglot && balcony == x.Balcony && elevator == x.Elevator && stor == x.Store &&  x.Adtype == AdType).Select(x => new { x.City, x.Neighborhood, x.Title, x.Age, x.Roomnum, x.Size, x.Price, x.RentPrice, x.Adtype, x.Usage, x.Parkinglot, x.Balcony, x.Elevator, x.Store }).ToList();
+
+            if(minsize != null)
+            {
+                homes= context.Homes.Where(x => (x.City == place || x.Neighborhood == place) && minsize <= x.Size && x.Size <= maxsize && parking == x.Parkinglot && balcony == x.Balcony && elevator == x.Elevator && stor == x.Store).Select(x => new { x.City, x.Neighborhood, x.Title, x.Age, x.Roomnum, x.Size, x.Price, x.RentPrice, x.Adtype, x.Usage, x.Parkinglot, x.Balcony, x.Elevator, x.Store }).ToList();
+            }
+            if(minage != null)
+            {
+                homes = context.Homes.Where(x => (x.City == place || x.Neighborhood == place) && minsize <= x.Size && x.Size <= maxsize && minage <= x.Age && x.Age <= maxage && parking == x.Parkinglot && balcony == x.Balcony && elevator == x.Elevator && stor == x.Store).Select(x => new { x.City, x.Neighborhood, x.Title, x.Age, x.Roomnum, x.Size, x.Price, x.RentPrice, x.Adtype, x.Usage, x.Parkinglot, x.Balcony, x.Elevator, x.Store }).ToList();
+
+            }
+            if (minprice != null)
+            {
+                homes = context.Homes.Where(x => (x.City == place || x.Neighborhood == place) && minsize <= x.Size && x.Size <= maxsize && minage <= x.Age && x.Age <= maxage && minprice <= x.Price && x.Price <= maxprice && parking == x.Parkinglot && balcony == x.Balcony && elevator == x.Elevator && stor == x.Store).Select(x => new { x.City, x.Neighborhood, x.Title, x.Age, x.Roomnum, x.Size, x.Price, x.RentPrice, x.Adtype, x.Usage, x.Parkinglot, x.Balcony, x.Elevator, x.Store }).ToList();
+
+            }
+            if (roomnum != null)
+            {
+                homes = context.Homes.Where(x => (x.City == place || x.Neighborhood == place) && minsize <= x.Size && x.Size <= maxsize && minage <= x.Age && x.Age <= maxage && minprice <= x.Price && x.Price <= maxprice && roomnum == x.Roomnum && parking == x.Parkinglot && balcony == x.Balcony && elevator == x.Elevator && stor == x.Store).Select(x => new { x.City, x.Neighborhood, x.Title, x.Age, x.Roomnum, x.Size, x.Price, x.RentPrice, x.Adtype, x.Usage, x.Parkinglot, x.Balcony, x.Elevator, x.Store }).ToList();
+
+            }
+            return Json(homes, JsonRequestBehavior.AllowGet);
+        }
+        //[HttpPost]
+        public ActionResult filter2(string place)
+        {
+
+            var hs = context.Homes.Where(x => x.City == place).SingleOrDefault();
+            //Select(x => new { x.City, x.Neighborhood, x.Title, x.Age, x.Roomnum, x.Size, x.Price, x.RentPrice, x.Adtype, x.Usage, x.Parkinglot, x.Balcony, x.Elevator, x.Store }).ToList();
+
+            return Json(hs, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult setagency(string AgName,string Agphone,string AgPass,string Agn,string Agp,string Agcity,int AgW)
+        {
+            var statee = 0;
+            var c = context.Agancies.Where(x => x.AcancyPhone == Agp ).SingleOrDefault();
+            if (c == null)
+            {
+                statee = 1;
+                Agancy newagancy = new Agancy();
+                newagancy.ManagerName = AgName;
+                newagancy.ManagerPhoneNumber = Agphone;
+                newagancy.Password = AgPass;
+                newagancy.AgancyName = Agn;
+                newagancy.AcancyPhone = Agp;
+                newagancy.City = Agcity;
+                newagancy.NumberofEmployees= AgW;
+
+                context.Agancies.Add(newagancy);
+                context.SaveChanges();
+
+            }
+
+            return Json(statee, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult setuser(string UserPhone)
+        {
+            var statee = 0;
+
+            var u = context.Users.Where(x => x.Phone == UserPhone).SingleOrDefault();
+
+            if (u == null)
+            {
+                statee = 1;
+
+                User newuser = new User();
+                newuser.Phone = UserPhone;
+
+                context.Users.Add(newuser);
+                context.SaveChanges();
+
+            }
+
+            return Json(statee, JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult edituser(string userP,string usname,string usemail,string uspassword)
+        {
+            var statee = 1;
+
+            var us = context.Users.Where(x => x.Phone == userP).SingleOrDefault();
+            us.Name = usname;
+            us.Password = uspassword;
+            us.Email = usemail;
+            context.SaveChanges();
+
+            if (us == null)
+            {
+                statee = 0;
+            }
+
+            return Json(statee, JsonRequestBehavior.AllowGet);
+        }
+
+       
+
+        public ActionResult setAD(int Adtype,int rentPrice,string city,string nhood,int size,int age,int price,int use,int roomnum,int parking,int elevator,int stor,int balcony,string title,string explain)
+        {
+            var statee = 0;
+
+            Home newhouse = new Home();
+            newhouse.Adtype = Adtype;
+            newhouse.RentPrice = rentPrice;
+            newhouse.City = city;
+            newhouse.Neighborhood = nhood;
+            newhouse.Size = size;
+            newhouse.Age = age;
+            newhouse.Price = price;
+            newhouse.Usage = use;
+            newhouse.Roomnum= roomnum;
+            newhouse.Parkinglot = parking;
+            newhouse.Elevator= elevator;
+            newhouse.Store = stor;
+            newhouse.Balcony = balcony;
+            newhouse.Title = title;
+            newhouse.Explain = explain;
+
+            context.Homes.Add(newhouse);
+            context.SaveChanges();
+
+            var newAD = context.Advertises.Where(x => x.hID == null).SingleOrDefault();
+            newAD.hID = newhouse.ID;
+      
+            context.SaveChanges();
+
+            statee = 1;
+
+            return Json(statee, JsonRequestBehavior.AllowGet);
+
+        }
+        [HttpPost]
+        public ActionResult getAgencies()
+        {
+            var dep = context.Agancies.Select(x => new {x.ID, x.AgancyName,x.AcancyPhone, x.ManagerName }).ToList();
+
+            return Json(dep, JsonRequestBehavior.AllowGet);
+        }
+
+
+
+    }
+}
---
+++ Kilidsite/assets/js/myjquery.js
@@ -0,0 +1,192 @@
+﻿function searching() {
+    alert("سلام"); 
+    var place = $("#place1").val();
+    var AdType = 0;
+
+    var size = $("#size1").val().split("_");
+
+
+    var age = $("#age1").val().split("_");
+
+    var price = $("#price1").val().split("_");
+
+    var roomnum = $("#roomnum1").val();
+
+    if ($("#size1").attr("selectedIndex") == 0) {
+        size = null;
+    }
+    if ($("#age1").attr("selectedIndex") == 0) {
+        age = null;
+    }
+    if ($("#price1").attr("selectedIndex") == 0) {
+        price = null;
+    }
+    if ($("#price1").attr("selectedIndex") == 0) {
+        roomnum = null;
+    }
+    var elevator = 0;
+
+    var paking = 0;
+
+    var balcony = 0;
+
+    var stor = 0;
+
+    if ($('#elevator1').is(":checked")) {
+        elevator = 1;
+    }
+    if ($('#parking1').is(":checked")) {
+        parking = 1;
+    }
+    if ($('#balcony1').is(":checked")) {
+        balcony = 1;
+    }
+    if ($('#stor1').is(":checked")) {
+        stor = 1;
+    }
+    
+    $.post("/Home/filterBuy", { AdType: AdType, place: place, minsize: size[0], maxsize: size[1], minage: age[0], maxage: age[1], minprice: price[0], maxprice: price[1], roomnum: roomnum, elevator: elevator, paking: paking, balcony: balcony, stor: stor })
+
+    $("#resaults").empty();
+
+  .done(function (res) {
+        for (var item in res) {
+            var par = "پارکینگ ندارد";
+            var ele = "آسانسور ندارد";
+            var bal = "بالکن ندارد";
+            var sto = "انباری ندارد";
+
+
+            if (res[item].ParkingLot == 1) {
+                par = "پارکینگ دارد"
+            }
+            if (res[item].Balcony == 1) {
+                bal = "بالکن دارد"
+            }
+            if (res[item].Elevator == 1) {
+                ele = "آسانسور دارد"
+            }
+            if (res[item].Store == 1) {
+                sto = "انباری دارد"
+            }
+
+            $("#resaults").append(
+
+                "<div class='col - lg - 4' data-aos='fade - up' data-aos-delay='100'>" +
+                "<div class='pricing - item'>" +
+                "<h3>" + res[item].Title + "</h3>" +
+                "<h4>" + res[item].City + "/" + res[item].Neighborhood + "</h4>" +
+                +"<ul>" +
+                "<li><i class='bi bi - check'></i>" + " قیمت " + res[item].Price + "</li>" +
+                "<li><i class='bi bi - check'></i>" + "متراژ" + res[item].Size + "</li>" +
+                "<li><i class='bi bi - check'></i>" + "سن" + res[item].Age + "</li>" +
+                "<li class='na'><i class='bi bi - x'></i> <span>" + "تعداد اتاق" + res[item].RoomNum + "</span></li>" +
+                "<li class='na'><i class='bi bi - x'></i> <span>" + "کاربری" + res[item].Usage + "</span></li>" +
+                "<li class='na'><i class='bi bi - x'></i> " + par + "/" + ele + "/" + bal + "/" + sto + "</li>" +
+                "</ul>" +
+                "<a href='' class='buy - btn'>" + خرید + "</a>" +
+                "</div>" +
+                "</div>"
+            )
+        }
+
+        $("#addsession").modal('show');
+    })
+
+        .fail(function () {
+
+
+        })
+        .always(function () {
+
+        });
+
+
+}
+
+function searching2() {
+    var place = $("#place2").val();
+    alert(place);
+    $.post("/Home/filter2", { place: place })
+
+        .done(function (res) {
+            for (var item in res) {
+
+                $("#resaults").append(
+
+                    "<div class='col - lg - 4' data-aos='fade - up' data-aos-delay='100'>" +
+                    "<div class='pricing - item'>" +
+                    "<h3>" + res[item].Title + "</h3>" +
+                    "<h4>" + res[item].City + "/" + res[item].Neighborhood + "</h4>" +
+                    +"<ul>" +
+                    "<li><i class='bi bi - check'></i>" + " قیمت " + res[item].Price + "</li>" +
+                    "<li><i class='bi bi - check'></i>" + "متراژ" + res[item].Size + "</li>" +
+                    "<li><i class='bi bi - check'></i>" + "سن" + res[item].Age + "</li>" +
+                    "<li class='na'><i class='bi bi - x'></i> <span>" + "تعداد اتاق" + res[item].RoomNum + "</span></li>" +
+                    "<li class='na'><i class='bi bi - x'></i> <span>" + "کاربری" + res[item].Usage + "</span></li>" +
+                    "<li class='na'><i class='bi bi - x'></i> " + par + "/" + ele + "/" + bal + "/" + sto + "</li>" +
+                    "</ul>" +
+                    "<a href='' class='buy - btn'>" + خرید + "</a>" +
+                    "</div>" +
+                    "</div>"
+                )
+            }
+
+
+        })
+        .fail(function () {
+            alert("خطا در برقراری ارتباط با سرور");
+        })
+        .always(function () {
+
+        });
+
+}
+
+
+$(document).ready(function () {
+    alert("ss")
+    $.post("/Home/getAgencies")
+
+        .done(function (res) {
+
+            $("#Agancies").empty();
+
+            for (var item in res) {
+
+                $("#Agancies").append(
+
+                    "<div class='swiper-slide'>" +
+                    '<div class="testimonial-item">' +
+                    '<img src="" class="testimonial-img" alt="">' +
+                    '<h3>' + res[item].AgancyName + '</h3>' +
+                    '<h4>' + res[item].AcancyPhone + '</h4>' +
+                    '<h5>' + res[item].ManagerName + '</h5>' +
+                    '<div class="stars">' +
+
+                    '</div>' +
+                    "<a href='' class='buy - btn'  id='Ag_" + res[item].ID + "'>" + آگهی + "</a>" +
+                    '</div>' +
+                    '</div>' +
+
+
+
+
+                    );
+            }
+
+
+        })
+
+        .fail(function () {
+
+
+
+        })
+
+        .always(function () {
+
+
+        });
+
+});
